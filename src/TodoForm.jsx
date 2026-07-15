bb827242fecc02d311ab7d0d91737cbdb69fec59
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-07-15 10:17:34 +0530

    revision modifications

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,13 @@
 function TodoForm() {
     return (
-    <form>
-      <label htmlFor="todoTitle">Todo</label>
-      <input type="text" id="todoTitle" />
-      <button type="submit" disabled>Add Todo</button>
-    </form>
+      <>
+        <form>
+          <label htmlFor="todoTitle">Todo</label>
+          <input type="text" id="todoTitle" />
+          <button type="submit" disabled>Add Todo</button>
+        </form>
+      </>
+    
     );
   }
   
