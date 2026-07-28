fee13c0abf909dfc79dcefc9954bb3b4c0fb132c
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-07-28 15:33:36 +0530

    adding events and handlers

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
 
-const todos = [
-  {id: 1, title: "review resources"},
-  {id: 2, title: "take notes"},
-  {id: 3, title: "code out app"}
-];
+// const todos = [
+//   {id: 1, title: "review resources"},
+//   {id: 2, title: "take notes"},
+//   {id: 3, title: "code out app"}
+// ];
 
 function App() {
 
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle){
+
+    const newTodo = {
+      id : Date.now(),
+      title : todoTitle
+    };
+
+    setTodoList(previous => [newTodo, ...previous]);
+
+  }
 
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
-      <TodoList todoList = {todoList} />
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
