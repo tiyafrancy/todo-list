35121763e8639d64a243d882f4d88c4044e71e23
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-08-04 10:04:07 +0530

    adding todo completion feature

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,31 +1,23 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function TodoForm({onAddTodo}) {
 
   const inputRef = useRef();
+  const [workingTodoTitle, setWorkingTodotitle] = useState("");
 
   const handleAddTodo = (event) => {
     event.preventDefault();
 
-    // console.log("Event object:", event);
-    // console.log("Event target:", event.target);
-    // console.log("Input value:", event.target.todoTitle.value);
+    const trimmedTitle = workingTodoTitle.trim();
 
-    const todoTitle = event.target.todoTitle.value.trim();
-    if(todoTitle && todoTitle !== "") {
-      onAddTodo(todoTitle);
-      event.target.reset();
+    if(trimmedTitle !== ""){
+      onAddTodo(trimmedTitle);
+      setWorkingTodotitle("");
       inputRef.current.focus();
     }
   };
 
     return (
-      // <form>
-      //   <label htmlFor="todoTitle">Todo</label>
-      //   <input type="text" id="todoTitle" />
-      //   <button type="submit" disabled>Add Todo</button>
-      // </form>  
-
       <form onSubmit = {handleAddTodo}>
         <label htmlFor = "todoTitle">Todo</label>
         <input
@@ -34,9 +26,11 @@ function TodoForm({onAddTodo}) {
           id = "todoTitle"
           name = "todoTitle"
           placeholder = {"Todo text"}
+          value={workingTodoTitle}
+          onChange={(event) => setWorkingTodotitle(event.target.value)}
           required
         />
-        <button type = "submit">
+        <button type = "submit" disabled={!workingTodoTitle.trim()} >
           Add Todo
         </button>
       </form>
