0870151a3d28b1b8322dd75fbd7b93dff4312c22
Tiya Francy Arangassery <192169708+tiyafrancy@users.noreply.github.com>
2026-09-10 22:11:29 -0500

    adding RequireAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router';
-import Header from './shared/Header.jsx';
-
 import HomePage from './pages/HomePage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import TodosPage from './pages/TodosPage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
-import TodosPage from './pages/TodosPage.jsx';
+import RequireAuth from './components/RequireAuth.jsx';
+import Header from './shared/Header.jsx';
+
 
 function App() {
 
@@ -16,12 +17,24 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/about" element={<AboutPage />} />
-
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/todos" element={<TodosPage />} />
-
+        <Route path="/login" element={<LoginPage />} />
+        <Route 
+          path="/todos" 
+          element={
+            <RequireAuth>
+              <TodosPage />
+            </RequireAuth>
+          }
+        />
+        <Route 
+          path="/profile" 
+          element={
+            <RequireAuth>
+              <ProfilePage />
+            </RequireAuth>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext.jsx";
+
+function RequireAuth({ children }) {
+    const { isAuthenticated } = useAuth();
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate('/login', { state: { from: location }, replace: true });
+        }
+    }, [isAuthenticated, location, navigate]);
+
+    if (!isAuthenticated) {
+        return (
+            <div>
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
+    return children;
+}
+
+export default RequireAuth;
